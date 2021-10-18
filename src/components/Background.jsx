0507ca0d01f442d1ae59e83a95d2c90dbfdba5ba
Isaac Moreno <ipdxcreative@gmail.com>
2021-10-18 13:54:27 -0700

    remove git lfs

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,13 +1,14 @@
-// import Earth from '../video/EarthFromSpace.mp4'
+import Earth from '../video/EarthFromSpace.mp4'
+
 import styles from '../styles/utils.module.css'
-import Earth from '../assets/nasa.jpg'
+// import Earth from '../assets/nasa.jpg'
 
 const Background = () => {
   return (
-    // <video autoPlay loop muted className={styles.backgroundVideo}>
-    //   <source src={Earth} type='video/mp4' />
-    // </video>
-    <img src={Earth} className={styles.backgroundVideo}/>
+    <video autoPlay loop muted className={styles.backgroundVideo}>
+      <source src={Earth} type='video/mp4' />
+    </video>
+    // <img src={Earth} alt='Satellite orbiting Earth' className={styles.backgroundVideo}/>
   )
 }
 
